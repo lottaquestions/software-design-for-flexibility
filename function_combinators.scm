1ef2a58544f269102ae10addc172929b0f68789d
lottaquestions <nickothieno@yahoo.com>
2022-06-28 19:49:09 -0400

    Completion of Multiple Values section of chapter 2

--- function_combinators.scm
+++ function_combinators.scm
@@ -12,12 +12,19 @@
 
 (define arity-table (make-key-weak-eqv-hash-table))
 
+(define max-possible-arity 256)
+
 (define (get-arity proc)
   (or (hash-table-ref/default arity-table proc #f)
       (let ((a (procedure-arity proc))) ; arity not in table
-	(assert (eqv? (procedure-arity-min a)
-		      (procedure-arity-max a)))
-	(procedure-arity-min a))))
+	;;(display a)
+	(if (not (procedure-arity-max a))
+	    max-possible-arity
+	    (begin
+	      (assert (eqv? (procedure-arity-min a)
+			    (procedure-arity-max a)))
+	      (procedure-arity-min a))
+	))))
 
 (define (compose f g)
   (let ((n (get-arity f)) (m (get-arity g)))
@@ -69,7 +76,7 @@
 
 
 
-
+;; Test code
 ((spread-combine list
 		   (lambda (x y) (list 'foo x y))
 	           (lambda (u v w) (list 'bar u v w)))
@@ -78,3 +85,71 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Multiple values
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (spread-apply f g)
+  (let ((n (get-arity f)) (m (get-arity g)))
+    (let ((t (+ n m)))
+      (define (the-combination . args)
+	(assert (= (length args) t))
+	(values (apply f (list-head args n))
+		(apply g (list-tail args n))))
+      (restrict-arity the-combination t))))
+
+(define (spread-combine h f g)
+  (compose h (spread-apply f g)))
+
+(define (compose f g)
+  (define (the-composition . args)
+    (call-with-values (lambda () (apply g args))
+      f))
+  (restrict-arity the-composition (get-arity g)))
+
+
+;; Test code
+((spread-combine list
+		   (lambda (x y) (list 'foo x y))
+	           (lambda (u v w) (list 'bar u v w)))
+ 'a 'b 'c 'd 'e)
+;Value: ((foo a b) (bar c d e))
+
+;; Generalizing further and allowing all the functions we are combining to return multiple values
+(define (spread-apply f g)
+  (let ((n (get-arity f)) (m (get-arity g)))
+    (let ((t (+ n m)))
+      (define (the-combination . args)
+	(assert (= (length args) t))
+	(let-values ((fv (apply f (list-head args n)))
+		     (gv (apply g (list-tail args n))))
+	  (apply values (append fv gv))))
+      (restrict-arity the-combination t))))
+
+;;Test
+((spread-combine list
+		 (lambda (x y) (values x y))
+		 (lambda (u v w) (values w v u)))
+ 'a 'b 'c 'd 'e)
+;;Value: (a b e d c)
+
+;; Reformulation of parallel combine to be a composition of two parts and to allow the
+;; parts to return multiple values
+
+(define (parallel-apply f g)
+  (let ((n (get-arity f)) (m (get-arity g)))
+    (let ((t (+ n m)))
+      (define (the-combination . args)
+	(assert (= (length args) m))
+	(assert (= m n))
+	(values (apply f args) (apply g args)))
+
+      (restrict-arity the-combination m))))
+
+(define (parallel-combine h f g)
+  (compose h (parallel-apply f g)))
+
+;;Test
+((parallel-combine list
+		   (lambda (x y z) (list 'foo x y z))
+	           (lambda (u v w) (list 'bar u v w)))
+ 'a 'b 'c)
+
+;;Value: ((foo a b c) (bar a b c))
